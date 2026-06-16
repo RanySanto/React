ec468966ad966702bbc5e1d76d9e926e4af32979
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-06-16 16:02:06 -0300

    Add files via upload

---
+++ first-react/src/components/Main.jsx
@@ -0,0 +1,16 @@
+function Main(){
+    return (
+    <main>
+        <h1>Fun facts about React</h1>
+        <ul className="facts-list">
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+        </ul>
+    </main>
+    )
+}
+
+export default Main
---
+++ first-react/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+        <header>
+            <nav>
+                <img src="/src/assets/react.svg" alt="" /> 
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    
+    )
+}
+
+export default Navbar
