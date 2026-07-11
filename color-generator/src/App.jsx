46c2e6220ec7b1c088c516b9e2f034cf6b2d9436
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-07-10 23:40:18 -0300

    Add files via upload

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [color, setColor] = useState()
@@ -7,26 +7,35 @@ function App() {
   useEffect(()=>{
   }, [])
 
+  function randomColorUtility(length){
+    return Math.floor(Math.random() * length)
+  }
+
   function handleClick(){
-    const validCharacters = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"]
-    let hexColor = ""
+    const hex = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"]
+    let hexColor = "#"
     for (let i = 0; i < 6; i++){
-      hexColor += validCharacters[Math.floor(Math.random() * validCharacters.length)]
+      hexColor += hex[randomColorUtility(hex.length)]
     }
     setColor(hexColor)
   }
 
-  function changeColorFormat(){
-    setColor((value)=>{value ? RGB : HEX ? HEX : RGB})
+  function changeColorFormat(color){
+    const r = randomColorUtility(256)
+    const g = randomColorUtility(256)
+    const b = randomColorUtility(256)
+    
+    // setColor(`rgb(${r},${g},${b})`)
+    setColor(`rgb(${r},${g},${b})`)
   }
 
   return (
-    <div id='color-container' style={{backgroundColor:`#${color}`}}>
+    <div id='color-container' style={{backgroundColor:`${color}`}}>
       <h2>HEX COLOR</h2>
-      <h2>{`#${color}`}</h2>
+      <h2>{`${color}`}</h2>
       <button onClick={handleClick}>Random Color</button>
-      {/* <button onClick={()=>{changeColorFormat()}}>RGB</button>
-      <button onClick={()=>{changeColorFormat()}}>HEX</button> */}
+      <button onClick={()=>{changeColorFormat(color)}}>RGB</button>
+      <button onClick={()=>{handleClick()}}>HEX</button>
     </div>
   )
 }
