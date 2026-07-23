ae71baaedf818efb6267789ec6f435267888a79b
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-07-22 23:48:10 -0300

    Update index.jsx

--- login/components/index.jsx
+++ login/components/index.jsx
@@ -1,39 +1,47 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 import Login from "./Login"
 import Register from "./Register"
 export default function LoginPage() {
     const [isLogged, setIsLogged] = useState(false)
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        localStorage.setItem("data", JSON.stringify(data))
+    }, [data])
+    useEffect(()=>{
+        const savedData = localStorage.getItem("data")
+        if (savedData) {
+            setData(JSON.parse(savedData))
+        }
+    },[])
+
     function handleSubmit(formData){
        const username = formData.get("username")
        const email = formData.get("email")
        const password = formData.get("password")
-       setData(
+       setData(prev=> [
+        ...prev,
         {
         "username":username,
         "email":email,
         "password":password
-       })
+       }])
        setIsLogged(true)
-       console.log(isLogged)
     }
-    function verifyAcc(){
-
-        // verify if current form has a match in the database
+    
+    function flipIsLogged(){
+        setIsLogged(prevIsLogged=>!prevIsLogged)
     }
-    function registerAccount(){
-        
-       
-       }
     return (
         <>
         {
         isLogged 
         ? (<Login 
+            return={flipIsLogged}
             data={data}
         /> )
         : (<Register
+            return={flipIsLogged}
             handleSubmit={handleSubmit}
         />)}
         </>
