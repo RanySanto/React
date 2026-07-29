258b6217359c3e30610ff5bb09b2e856b58d6c4b
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-07-28 23:22:56 -0300

    Add files via upload

---
+++ Planner/src/Planner.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+
+function Planner() {
+
+  return (
+    <>
+      <div className='task-container'>
+        <div className='tasks-header'><p>Morning Workout</p> <span className='streak'>1</span></div>
+        <div className='week-container'>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'>✓</div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        </div>
+      </div>
+      <div className='task-container'>
+        <div className='tasks-header'><p>Drink 8 glasses of water</p> <span className='streak'>1</span></div>
+        <div className='week-container'>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'>✓</div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        <div className='task-cont'>
+          <p>MO</p>
+          <div className='task-div'></div>
+        </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Planner
---
+++ Planner/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Planner from './Planner.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Planner />
+  </StrictMode>,
+)
