ff8a537e765c4c3a4b88d160e2b105e2baf19319
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-08-04 23:34:03 -0300

    Update Task.jsx

--- Planner/src/Task.jsx
+++ Planner/src/Task.jsx
@@ -5,12 +5,22 @@ function Task(props) {
   // função pra criar uma task
   // a cor é definida aleatoriamente
   
+      const [on, setOn] = useState(false)
+    function toggle(){
+        // setOn(prevOn => !prevOn)
+        console.log("clicked")
+    }
 
-  const [days, setDays] = useState([{day:'MO', on:true, id:1}, {day:'TUE', on:false, id:2}, {day:'WED', on:false, id:3}, {day:'THUR', on:false, id:4}, {day:'FRI', on:false, id:5}, {day:'SAT', on:false, id:6}, {day:'SUN', on:false, id:7}])
+  
+
+  const [days, setDays] = useState([{day:'MO', active:false, id:1}, {day:'TUE', active:false, id:2}, {day:'WED', active:false, id:3}, {day:'THUR', active:false, id:4}, {day:'FRI', active:false, id:5}, {day:'SAT', active:false, id:6}, {day:'SUN', active:false, id:7}])
 
   const daysElement = days.map(day =>
-    <Day id={day.id} day={day.day} on={day.on} styles={{backgroundColor: day.on ? props.color : "#040b15"}}/>
+    <Day toggle={toggle} id={day.id} day={day.day} active={day.active} color={props.color}/>
   )
+
+   
+  //   
 
   return (
     <>
