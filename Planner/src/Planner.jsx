e313ae61884acc8f7dfa64b2d92968200b88196a
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-08-04 23:33:46 -0300

    Update Planner.jsx

--- Planner/src/Planner.jsx
+++ Planner/src/Planner.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react'
 import Task from './Task'
 import Calendar from './Calendar'
 
-function Planner() {
- 
 
+function Planner() {
+  const [tasks, setTasks] = useState([
+    <Task taskName={"Workout"} color={"#2aac78"}/>
+  ])
+ const taskColor = ["#e08d79", "#A882DD", "#B3F2DD", "#2aac78", "#84E296", "#FF5666", "#FFC857"]
+ function getTaskColor(){
+  return taskColor[Math.floor(Math.random() * taskColor.length)]
+ }
+ getTaskColor()
+  const nameTask = "Study"
+  function addTask(){
+    setTasks(prevTasks=>[...prevTasks, <Task key={nameTask} taskName={nameTask} color={getTaskColor()}/>])
+  }
 
   return (
-    <>
-      {/* <Task taskName={"Workout"} color={"#2aac78"}/>
-      <Task taskName={"Drawing"} color={"#0db5db"}/> */}
-      <Calendar/>
-      
-      
-    </>
+    <div className='container'>
+      <p><button>{"<"}</button> July 1-7 <button>{">"}</button> </p>
+    {/* <Calendar/> */}
+      {tasks}
+      <div className='task-container' onClick={addTask}><button>+</button> add new task</div>
+    </div>
   )
 }
 
