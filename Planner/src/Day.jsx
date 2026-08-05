f56a1befee67713dc6bd51ba9edbd7c67f2f7ef0
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-08-04 23:34:13 -0300

    Update Day.jsx

--- Planner/src/Day.jsx
+++ Planner/src/Day.jsx
@@ -1,15 +1,11 @@
 import {useState, useEffect} from "react"
 export default function Day(props){
-    const [on, setOn] = useState(props.on)
-    function toggle(){
-        setOn(prevOn => !prevOn)
-    }
-   
-    
+
+
     return(
         <div className='task-cont'>
           <p>{props.day}</p>
-          <div className={on ? "task-div on": "task-div"} key={props.day} id={props.id} onClick={toggle} ></div>
+          <div className={props.active ? "task-div on": "task-div"} key={props.day} id={props.id} onClick={props.toggle} style={{backgroundColor: props.active ? props.color : "#040b15"}}></div>
         </div>
     )
 }
