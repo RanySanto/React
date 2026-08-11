a91179f7296834a74ecf686a52aecb1554dac332
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-08-10 23:06:57 -0300

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,29 @@
+const api_key = "1a0f7b2c79d87124b7ecd742c96174f3"
+
+
+const local = document.getElementById("local")
+const temp = document.getElementById("temp")
+const current_day = document.getElementById("current-day")
+const wind_speed = document.getElementById("wind-speed")
+const humidity = document.getElementById("humidity")
+const feels_like = document.getElementById("feels-like")
+const climate = document.getElementById("climate")
+
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${api_key}`)
+    .then(response => response.json())
+    .then(data=>{
+        local.textContent = data.name
+        temp.textContent = (Math.floor(data.main.temp -273.15))+"°"
+            wind_speed.textContent = data.wind.speed + " km"
+            climate.textContent = (data.weather[0].description)
+            // // data.weather.main.icon,
+            humidity.textContent = data.main.humidity
+        
+    })
+    .catch(error=>{
+        console.error(error)
+    })
+
+console.log(new Date())
